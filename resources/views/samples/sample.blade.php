f6752f9ec482c271316654af39390a54f2d7ea01
Alifa Hafida Anwar <alifahafida@gmail.com>
2023-06-21 00:51:20 +0700

    sample gallery info change, edit profile change

--- resources/views/samples/sample.blade.php
+++ resources/views/samples/sample.blade.php
@@ -4,25 +4,12 @@
 
 <section class="text-center pb-24">
     <h1 class="font-montserrat font-extrabold text-3xl">Contoh Sampel</h1>
-    <div class="pt-8">
-        <span>
-          <!-- <form action="/sampel">
-            <input type="text" id="search" class="h-12 w-1/2 p-3 rounded-lg shadow-md border-2" name="search" value="{{ request('search') }}">
-            <span class="pl-3 space-x-3">
-              <button type="submit" class="w-20 h-12 text-center rounded-lg bg-dongker text-white text-base hover:bg-dongker/50">
-                Cari
-              </button>
-            </span>
-          </form> -->
-        </span>
-
-    </div>
 </section>
 
 <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
       @foreach($samples as $sample)
-      <div class="container mx-auto px-4 aspect-square mb-4">
-          <img class="h-auto max-w-full rounded-lg" src="{{ asset('storage/' . $sample->photo )}}" alt="">
+      <div class="container mx-auto px-4 aspect-square">
+          <img class="h-auto max-w-full rounded-lg" src="{{ asset('storage/' . $sample->photo )}}" title="{{$sample->productName}}" alt="sampel">
       </div>
       @endforeach
 </div>
